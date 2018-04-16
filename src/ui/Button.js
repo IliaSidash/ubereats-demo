cad598bf94fc763e8bc814c0d2ecf359f0f84a83
iliasidash <iliasidash@gmail.com>
2018-04-16 16:51:29 +0500

    feat: add Button

---
+++ src/ui/Button.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  width: 72px;
+  height: 40px;
+  background-color: transparent;
+  font-weight: 500;
+  line-height: normal;
+  font-size: 11px;
+
+  cursor: pointer;
+`;
+
+export default Button;
